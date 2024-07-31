350ce30a4ad5907f9fa64279fa650cd1a0d5c13c
JacobPaff <20961612@students.latrobe.edu.au>
2024-07-31 10:59:04 +0000

    Register Model with Admin

--- cse3cax_webapp/admin_user_input/admin.py
+++ cse3cax_webapp/admin_user_input/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, Role, Subject, SubjectInstance
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Role)
+admin.site.register(Subject)
+admin.site.register(SubjectInstance)
