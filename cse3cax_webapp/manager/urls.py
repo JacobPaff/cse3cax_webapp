9da25b3bfa4a5b2fbeba1a86e68d63ec3f65fb18
JacobPaff <20961612@students.latrobe.edu.au>
2024-09-19 06:25:39 +0000

    Assign Lecturers Button

--- cse3cax_webapp/manager/urls.py
+++ cse3cax_webapp/manager/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('instance_list/', views.instance_list, name='instance_list'),
     path('add_subject_instance/', views.add_subject_instance, name='add_subject_instance'),
     path('edit_subject_instance/<int:instance_id>', views.edit_subject_instance, name='edit_subject_instance'),
+    path('assign_lecturer_instance/', views.assign_lecturer_instance, name='assign_lecturer_instance'),
+    path('lecturer_list/', views.lecturer_list, name='lecturer_list'),
     path('confirm_delete_instance/<int:instance_id>', views.confirm_delete_instance, name='confirm_delete_instance'),
     path('delete_instance/<int:instance_id>/', views.delete_instance, name='delete_instance'),
 ]
--- cse3cax_webapp/manager/views.py
+++ cse3cax_webapp/manager/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from core.models import SubjectInstance,Subject
+from core.models import SubjectInstance, Subject, SubjectInstanceLecturer, UserProfile
 from .forms import SubjectInstanceForm
 from django.http import HttpResponse
 from django.urls import reverse
+from django.db.models import Q
+
 
 def subject_instances(request):
     subjects = Subject.objects.all()
@@ -12,7 +14,8 @@ def subject_instances(request):
 def instance_list(request):
     subject = request.GET.get('subject')
     if subject:
-        subject_instances = SubjectInstance.objects.filter(subject__subject_id=subject)
+        subject_instances = SubjectInstance.objects.filter(
+            subject__subject_id=subject)
     else:
         subject_instances = SubjectInstance.objects.all()
     return render(request, 'instance_list.html', {'subject_instances': subject_instances})
@@ -30,8 +33,10 @@ def add_subject_instance(request):
     form_string = 'Create Subject Instance'
     return render(request, 'modals/form_modal.html', {'form': form, 'form_string': form_string})
 
+
 def edit_subject_instance(request, instance_id):
-    subject_instance = get_object_or_404(SubjectInstance, instance_id=instance_id)
+    subject_instance = get_object_or_404(
+        SubjectInstance, instance_id=instance_id)
     if request.method == "POST":
         form = SubjectInstanceForm(request.POST, instance=subject_instance)
         if form.is_valid():
@@ -42,10 +47,12 @@ def edit_subject_instance(request, instance_id):
     form_string = 'Edit Subject Instance'
     return render(request, 'modals/form_modal.html', {'form': form, 'form_string': form_string})
 
+
 def confirm_delete_instance(request, instance_id):
     instance = get_object_or_404(SubjectInstance, instance_id=instance_id)
     form_string = f'Are you sure you want to delete instance {instance.subject}-{instance.month}/{instance.year}?'
-    delete_url = reverse("delete_instance", kwargs={"instance_id": instance.instance_id})
+    delete_url = reverse("delete_instance", kwargs={
+                         "instance_id": instance.instance_id})
     hx_post_attribute = f'hx-post="{delete_url}"'
     context = {
         'form_string': form_string,
@@ -53,10 +60,52 @@ def confirm_delete_instance(request, instance_id):
     }
     return render(request, 'modals/confirm_delete_modal.html', context)
 
+
 def delete_instance(request, instance_id):
-    subject_instance = get_object_or_404(SubjectInstance, instance_id=instance_id)
+    subject_instance = get_object_or_404(
+        SubjectInstance, instance_id=instance_id)
     subject_instance.delete()
-    return HttpResponse(status=204, headers={'Hx-Trigger':'instanceListChanged'})  # No content response
+    # No content response
+    return HttpResponse(status=204, headers={'Hx-Trigger': 'instanceListChanged'})
+
+
+def assign_lecturer_instance(request):
+    instance_id = request.GET.get('instance_id')
+    return render(request, 'assign_lecturer_instance.html', {'instance_id': instance_id})
+
+
+def lecturer_list(request):
+    query = request.GET.get('search', '')
+    instance_id = request.GET.get('instance_id')
+
+    # Get the subject instance
+    subject_instance = SubjectInstance.objects.get(pk=instance_id)
+
+    # Get lecturers assigned to the subject instance
+    assigned_lecturers = UserProfile.objects.filter(
+        subjectinstancelecturer__subject_instance=instance_id)
+    # assigned_lecturers = UserProfile.objects.all()
+    # Get lecturers who have expertise in the subject of this instance
+    expertised_lecturers = UserProfile.objects.filter(
+        lecturerexpertise__subject=subject_instance.subject
+    )
+    # expertised_lecturers = UserProfile.objects.all()
+
+    # Combine both filters: lecturers with expertise and assigned to the subject instance
+    user_list = expertised_lecturers
+
+    # Can this .... for now
+    # Apply the search query to filter by first or last name
+    if query:
+        user_list = user_list.filter(
+            Q(first_name__icontains=query) | Q(
+                last_name__icontains=query)
+        )
+
+    return render(request, 'lecturer_list.html', {
+        'user_list': user_list,
+        'assigned_lecturers': assigned_lecturers
+    })
 
 # def confirm_delete_modal(request, user_id):
 #     user = get_object_or_404(UserProfile, user_id=user_id)
@@ -74,4 +123,3 @@ def delete_instance(request, instance_id):
 #         form = UserProfileForm(instance=user)
 #     form_string = f'Edit {user.first_name } { user.last_name}'
 #     return render(request, 'modals/form_modal.html', {'form': form, 'form_string': form_string})
-
